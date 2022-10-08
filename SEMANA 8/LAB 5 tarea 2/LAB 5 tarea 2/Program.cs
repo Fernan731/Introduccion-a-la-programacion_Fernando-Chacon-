55eb9fe10bddaafa12baa401e3f174c3e34585ff
Fernan731 <115184788+Fernan731@users.noreply.github.com>
2022-10-08 08:01:06 -0600

    m

---
+++ SEMANA 8/LAB 5 tarea 2/LAB 5 tarea 2/Program.cs
@@ -0,0 +1,33 @@
+﻿class programn
+{
+
+    static void Main(string[] args)
+    {
+
+
+        double monto;
+
+        Console.WriteLine("Ingrese el monto");
+        monto = Convert.ToDouble(Console.ReadLine());
+
+        if (monto >= 400 && monto <= 1000)
+        {
+            Console.WriteLine("7% de descuento");
+        }
+        else if (monto >= 1000 && monto <= 5000)
+        {
+            Console.WriteLine("10% de descuento");
+
+        }
+        else if (monto >= 5000 && monto <= 15000)
+        {
+            Console.WriteLine("15% en ventas");
+        }
+        else if (monto >= 15000)
+        {
+            Console.WriteLine("25% de descuento");
+        }
+        else if (monto < 400)
+        { Console.WriteLine("No hay descuento"); }
+    }
+}
